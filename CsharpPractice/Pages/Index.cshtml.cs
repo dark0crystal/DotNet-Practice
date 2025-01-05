3edc358fb7d65e00a27eafb390fa948531ad0d6e
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-05 18:56:07 +0400

    rock paper scissors index page

--- CsharpPractice/Pages/Index.cshtml.cs
+++ CsharpPractice/Pages/Index.cshtml.cs
@@ -12,13 +12,90 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
-    public void OnGet()
+    public string[] Array = new[] { "rock", "paper", "scissors" };
+    public string HumanChoice { get; set; } = string.Empty;
+    public string ComputerSelection { get; set; } = string.Empty;
+    
+    public void OnGet(string choice)
     {
         
+       
+        if (!string.IsNullOrEmpty(choice))
+        {
+            // Generate a random number between 0 and 2 (inclusive)
+            Random random = new Random();
+            int randomIndex = random.Next(0, Array.Length);
+
+            // Set human and computer choices
+            HumanChoice = choice;
+            ComputerSelection = Array[randomIndex];
+            if (HumanChoice == "rock" && ComputerSelection == "scissors")
+            {
+                ViewData["message"] = "You Win";
+                ViewData["computer"] = ComputerSelection;
+                ViewData["human"] = HumanChoice;
+            }
+            else if (HumanChoice == "rock" && ComputerSelection == "paper")
+            {
+                ViewData["message"] = "Computer Win";
+                ViewData["computer"] = ComputerSelection;
+                ViewData["human"] = HumanChoice;
+            }
+            else if (HumanChoice == "scissors" && ComputerSelection == "rock")
+            {
+                ViewData["message"] = "Computer Win";
+                ViewData["computer"] = ComputerSelection;
+                ViewData["human"] = HumanChoice;
+            }
+            else if (HumanChoice == "rock" && ComputerSelection == "scissors")
+            {
+                ViewData["message"] = "Computer Win";
+                ViewData["computer"] = ComputerSelection;
+                ViewData["human"] = HumanChoice;
+                
+            }
+            else if (HumanChoice == ComputerSelection )
+            {
+                ViewData["message"] = "Tie";
+                ViewData["computer"] = ComputerSelection;
+                ViewData["human"] = HumanChoice;
+                
+            }
+        }
+        
+        // if (choice !="")
+        //     
+        // {
+        //     switch (choice)
+        //     {
+        //         case "rock":
+        //             Console.WriteLine("rock");
+        //             this.computerSelection =this.array[randomNumber];
+        //             Console.WriteLine("computer",this.computerSelection);
+        //             humenChoice = "rock";
+        //             break;
+        //         case "paper":
+        //             Console.WriteLine("paper");
+        //             this.computerSelection =this.array[randomNumber];
+        //             Console.WriteLine("computer",this.computerSelection);
+        //             humenChoice = "paper";
+        //             break;
+        //         case "scissors":
+        //             Console.WriteLine("scissors");
+        //             this.computerSelection =this.array[randomNumber];
+        //             Console.WriteLine("computer",this.computerSelection);
+        //             humenChoice = "scissors";
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // }
+        //
+        //
+        //
+        //
+        // Console.WriteLine($"choice: {choice}");
     }
 
-    public void OnPost()
-    {
-        
-    }
+   
 }
